1ade9ffe4319e70fd889d6888fe88ec715723a63
Daniel Savescu <savescu.daniel.cristian@gmail.com>
2023-05-13 17:08:49 +0300

    write output to file

--- numberConverter.py
+++ numberConverter.py
@@ -8,13 +8,21 @@
 def decimalToBinary(number):
 	return bin(number)[2:].zfill(64)
 
+from datetime import date
+
 def decimalToHexazecimal(number):
     return hex(number)[2:].zfill(64)
     
 def decimalToOctal(number):
     return oct(number)[2:].zfill(64)
 
+#Get date for log file
 
+currentDay = date.today()
+
+#Creating log FIle
+
+logFile = open("log.txt", "a")
 
 #Prompt
 
@@ -28,3 +36,9 @@ print()
 print("The number {} in hexazecimal is {} on 64 bit".format(numberToBeConverted,decimalToHexazecimal(numberToBeConverted)))
 print()
 print("The number {} in octal is {} on 64 bit".format(numberToBeConverted,decimalToOctal(numberToBeConverted)))
+
+#Write to log file
+
+logFile.write("Date : {} | The number {} in binary is {} on 64 bit\n".format(currentDay, numberToBeConverted, decimalToBinary(numberToBeConverted)))
+logFile.write("Date : {} | The number {} in hexazecimal is {} on 64 bit\n".format(currentDay, numberToBeConverted, decimalToHexazecimal(numberToBeConverted)))
+logFile.write("Date : {} | The number {} in octal is {} on 64 bit\n".format(currentDay, numberToBeConverted, decimalToOctal(numberToBeConverted)))
