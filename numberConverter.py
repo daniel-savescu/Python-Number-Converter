3c00bac97b28be5a28670de33fb27ef24f4ecfc2
Daniel Savescu <savescu.daniel.cristian@gmail.com>
2023-05-14 16:07:29 +0300

    Added save ouput as txt and pdf

--- numberConverter.py
+++ numberConverter.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from fpdf import FPDF
+
 #Program : Decimal converter
 #Python 3.10.11
 
@@ -6,21 +9,25 @@
 #Function declaration
 
 def decimalToBinary(number):
-	return bin(number)[2:].zfill(64)
+	return bin(number)[2:].zfill(32)
 
 from datetime import date
 
 def decimalToHexazecimal(number):
-    return hex(number)[2:].zfill(64)
+    return hex(number)[2:].zfill(32)
     
 def decimalToOctal(number):
-    return oct(number)[2:].zfill(64)
+    return oct(number)[2:].zfill(32)
 
-#Get date for log file
-
+#Get date and time for log file
+now = datetime.now()
+currentTime = now.strftime("%H:%M:%S")
 currentDay = date.today()
 
-#Creating log FIle
+
+
+
+#Creating log File as txt
 
 logFile = open("log.txt", "a")
 
@@ -29,16 +36,41 @@ logFile = open("log.txt", "a")
 print("Enter a decimal number to be converted in:\n")
 print(7*"=" + " Binary | Hexazecimal | Octal" + 7*"=")
 print()
-numberToBeConverted = int(input("Enter a number to be converted: "))
-print()
-print("The number {} in binary is {} on 64 bit".format(numberToBeConverted,decimalToBinary(numberToBeConverted)))
-print()
-print("The number {} in hexazecimal is {} on 64 bit".format(numberToBeConverted,decimalToHexazecimal(numberToBeConverted)))
-print()
-print("The number {} in octal is {} on 64 bit".format(numberToBeConverted,decimalToOctal(numberToBeConverted)))
+try:
+    numberToBeConverted = int(input("Enter a number to be converted: "))
+    print()
+    print("The number {} in binary is {} on 64 bit".format(numberToBeConverted,decimalToBinary(numberToBeConverted)))
+    print()
+    print("The number {} in hexazecimal is {} on 64 bit".format(numberToBeConverted,decimalToHexazecimal(numberToBeConverted)))
+    print()
+    print("The number {} in octal is {} on 64 bit".format(numberToBeConverted,decimalToOctal(numberToBeConverted)))
 
-#Write to log file
+    #Write to log file
+    
+    logFile.write("Date : {} and Time {} | The number {} in binary is {} on 64 bit\n".format(currentDay, currentTime, numberToBeConverted, decimalToBinary(numberToBeConverted)))
+    logFile.write("Date : {} and Time {} | The number {} in hexazecimal is {} on 64 bit\n".format(currentDay, currentTime, numberToBeConverted, decimalToHexazecimal(numberToBeConverted)))
+    logFile.write("Date : {} and Time {} | The number {} in octal is {} on 64 bit\n".format(currentDay, currentTime, numberToBeConverted, decimalToOctal(numberToBeConverted)))
+    
+    #Set FPDF
 
-logFile.write("Date : {} | The number {} in binary is {} on 64 bit\n".format(currentDay, numberToBeConverted, decimalToBinary(numberToBeConverted)))
-logFile.write("Date : {} | The number {} in hexazecimal is {} on 64 bit\n".format(currentDay, numberToBeConverted, decimalToHexazecimal(numberToBeConverted)))
-logFile.write("Date : {} | The number {} in octal is {} on 64 bit\n".format(currentDay, numberToBeConverted, decimalToOctal(numberToBeConverted)))
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size = 6)
+
+    readTxtLogFile = open("log.txt", "r")
+
+    for x in readTxtLogFile:
+        pdf.cell(500, 15, txt = x, ln = 1, align = 'L')
+    
+    #Save the PDF with name .pdf
+    pdf.output("log_pdf.pdf")
+    
+    #close TXT File Object 
+    
+    logFile.close()
+    
+    print("The ouput is saved in txt and pdf format. Please check the output where is the script !")
+    
+except ValueError:
+    print("Wrong Input! Only Integers")
+
